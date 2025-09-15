a2f02ee05c5850fd8ccb0c59511445cf3cd946de
Bettrix <bettbrian022@gmail.com>
2025-09-16 00:40:16 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance<=400){
+    return("This one is on me!");
+    }
+  else if (distance > 400 && distance < 2000){
+    return("That will be twenty bucks.");
+    }
+  else if (distance > 2000 && distance<=2500){
+    return("I will gladly take your thirty bucks.");
+  }
+  else {
+    return("No can do.");
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message;
+  city==="NYC"? message="Ok, sounds good." : message="No go.";
+  return (message);
+    console.log(ternaryCheckCity(NYC));
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+      return("Thank you so much.");
+      break;
+    
+    case "not as generous":
+      return("Thank you.");
+      break;
+    default:
+      return("Bye.");
+  }
 }
+
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+ternaryCheckCity("NYC");
+switchOnCharmFromTip("generous");
+switchOnCharmFromTip("not as generous");
+switchOnCharmFromTip("thanks for everything");
